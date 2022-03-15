45af2c3f4c7f08cf2a1357526ddebae7f1243bd6
David Ram Menez <dcm.facul@gmail.com>
2022-03-14 23:25:41 -0300

    trabalhando formulas

---
+++ #Tests/Questions-08-03-2022/src/Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+			//4 - Faça um programa para calcular e mostrar a área de um cubo.
+				double aT, a;
+				
+				System.out.print("Informe o valor da aresta do cubo: \n", a);
+				a = sc.nextDouble();
+
+				//Área do Cubo tema Fórmula 
+				At = Math. pow((6*a), 2);
+				
+				System.out.print("A área total do cubo, equivale a: ", aT);
+		sc.close();
+	}
+}
+
+//1 - Fa�a um programa para  somar 3 n�meros inteiros quaisquer e mostrar o resultado.
+/*
+	int n1, n2, n3, resultado;
+
+	System.out.print("Digite o 1� n�mero inteiro: \n");
+	n1 = sc.nextInt();
+	System.out.print("Digite o 2� n�mero inteiro: \n");
+	n2 = sc.nextInt();
+	System.out.print("Digite o 3� n�mero inteiro: \n");
+	n3 = sc.nextInt();
+	
+	resultado = n1+n2+n3;
+	
+	System.out.printf("A soma dos 3 n�meros inteiros informados tem resultado de:\n %d", resultado);
+*/
+
+//2 - Fa�a um programa para calcular e mostrar a �rea de um quadrado.
+/*
+double lado, area;
+System.out.print("Informe a altura ou largura do quadrado: \n");
+lado = sc.nextDouble();
+
+area = Math. pow(lado, 2);
+
+System.out.printf("A �rea do quadrado �: %.2f", area);
+*/
+
+/*
+ * //3 - Fa�a um programa para calcular e mostrar a �rea de triangulo ret�ngulo.
+				double b, h, area;
+				System.out.print("Informe o valor da base do triangulo ret�ngulo: \n");
+				b = sc.nextDouble();
+				System.out.print("Informe o valor da altura do triangulo ret�ngulo: \n");
+				h = sc.nextDouble();
+				
+				area = (b*h)/2;
+				
+				System.out.printf("A �rea do triangulo ret�ngulo �: %.2f", area);
+ */
