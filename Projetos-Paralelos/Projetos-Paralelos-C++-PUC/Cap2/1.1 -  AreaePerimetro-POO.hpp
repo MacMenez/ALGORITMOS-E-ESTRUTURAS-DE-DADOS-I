43ac585233b7d8d88d241709c41e18daa39ce6b9
David Ram Menez <dcm.facul@gmail.com>
2022-04-10 07:43:23 -0300

    Update code

--- Projetos-Paralelos/Projetos-Paralelos-C++-PUC/Cap2/1.1 -  AreaePerimetro-POO.hpp
+++ Projetos-Paralelos/Projetos-Paralelos-C++-PUC/Cap2/1.1 -  AreaePerimetro-POO.hpp
@@ -5,17 +5,33 @@ private:
     float altura;
     float largura;
 public:
-    Retangulo(/* args */);
-    altura = 0;
-    largura = 0;
-    ~Retangulo();
+    Retangulo(){
+        altura = 0;
+        largura = 0;
+    }
+    
+    float getAltura(){
+        return altura;
+    }
+
+    void setAltura(float a){
+        altura = a;
+    }
+
+    float getLargura(){
+        return largura;
+    }
+
+    void setLargura(float l){
+        largura = 1;
+    }
+
+    float calculaArear(){
+        return altura * largura;
+    }
+
+    float calculaPerimetro(){
+        return 2 * altura + 2 * largura;
+    }
 
 };
-
-Retangulo::Retangulo(/* args */)
-{
-}
-
-Retangulo::~Retangulo()
-{
-}
